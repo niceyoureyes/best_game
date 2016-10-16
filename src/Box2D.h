e4ca0c19f3323d255fd6ef7bd16255d02ca54cd2
Vasco <vasx222@mail.ru>
2016-10-16 16:37:17 +0300

    changed

---
+++ src/Box2D.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+#include "Point2D.h"
+
+class Box2D
+{
+public:
+  Box2D(Point2D const &p1, Point2D const &p2);
+  Box2D(Box2D const &obj);
+  Box2D(Box2D && obj);
+  Box2D &operator = (Box2D && obj);
+  Box2D operator = (Box2D const &obj);
+  Point2D & point1();
+  Point2D & point2();
+  Point2D const & point1() const;
+  Point2D const & point2() const;
+private:
+  Point2D m_point1, m_point2;
+};
+std::ostream &operator << (std::ostream &os, Box2D const &box);
---
+++ src/Ray2D.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Point2D.h"
+
+class Ray2D
+{
+public:
+  Ray2D(Point2D const &origin, Point2D const &direction);
+  Ray2D(Ray2D const &obj);
+  Ray2D(Ray2D && obj);
+  Ray2D &operator = (Ray2D && obj);
+  Ray2D operator = (Ray2D const &obj);
+  Point2D &origin();
+  Point2D const &origin() const;
+  Point2D &direction();
+  Point2D const &direction() const;
+private:
+  Point2D m_origin, m_direction;
+};
+
+std::ostream &operator << (std::ostream &os, Ray2D const &obj);
